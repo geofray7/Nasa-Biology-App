12028458154043aeb7beeaa705c497ab203e65dd
Geofray Jacob <geofrayjacob@gmail.com>
2025-10-04 17:55:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3104674087-3cc8f",
+  "appId": "1:572605350883:web:5b2890f1ebf2234c5796d6",
+  "apiKey": "AIzaSyBvs_i6z-iLvcKms34uw8bQLPBD5sjc70w",
+  "authDomain": "studio-3104674087-3cc8f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "572605350883"
+};
