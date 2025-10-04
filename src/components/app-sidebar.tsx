8ce5a19bfc1ab5509e3675d3ce9ba49d4ae7a1cb
Geofray Jacob <geofrayjacob@gmail.com>
2025-10-04 15:49:56 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -67,15 +67,16 @@ export function AppSidebar() {
       <SidebarMenu className="flex-1">
         {navItems.map((item) => (
           <SidebarMenuItem key={item.href}>
-            <Link href={item.href} passHref legacyBehavior>
-              <SidebarMenuButton
-                isActive={pathname.startsWith(item.href)}
-                tooltip={item.label}
-              >
+            <SidebarMenuButton
+              asChild
+              isActive={pathname.startsWith(item.href)}
+              tooltip={item.label}
+            >
+              <Link href={item.href}>
                 <item.icon />
                 <span>{item.label}</span>
-              </SidebarMenuButton>
-            </Link>
+              </Link>
+            </SidebarMenuButton>
           </SidebarMenuItem>
         ))}
       </SidebarMenu>
